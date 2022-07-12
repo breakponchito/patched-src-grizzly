f9eaa3566eb597aa8f78e5962d52d3c54d4eac8b
luiseufrasio <luis.eufrasio@gmail.com>
2022-07-12 18:47:17 -0300

    FISH-1515 Connection timeout issue solved for Payara 6

--- modules/grizzly/src/main/java/org/glassfish/grizzly/utils/IdleTimeoutFilter.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/utils/IdleTimeoutFilter.java
@@ -30,6 +30,7 @@ import org.glassfish.grizzly.attributes.Attribute;
 import org.glassfish.grizzly.filterchain.BaseFilter;
 import org.glassfish.grizzly.filterchain.FilterChainContext;
 import org.glassfish.grizzly.filterchain.NextAction;
+import org.glassfish.grizzly.nio.NIOConnection;
 
 /**
  * The Filter is responsible for tracking {@link Connection} activity and closing {@link Connection} once it becomes
@@ -322,7 +323,11 @@ public class IdleTimeoutFilter extends BaseFilter {
                 if (handler != null) {
                     handler.onTimeout(connection);
                 }
-                connection.closeSilently();
+                if (connection instanceof NIOConnection) {
+                    return true;
+                } else {
+                    connection.closeSilently();
+                }
             }
 
             return true;
