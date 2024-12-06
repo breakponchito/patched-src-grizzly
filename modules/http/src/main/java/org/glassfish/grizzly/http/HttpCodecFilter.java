9d3794923f57af6374bbd9690f88c27c8f9af56e
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2024-12-06 12:07:31 -0600

    FISH-9690: removing unused import

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpCodecFilter.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpCodecFilter.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 import org.glassfish.grizzly.Buffer;
 import org.glassfish.grizzly.Connection;
 import org.glassfish.grizzly.Grizzly;
